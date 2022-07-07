97b07f8a6a83abb98e28f8dd1eca464b6e24133d
mirmik <mirmikns@yandex.ru>
2022-07-07 23:11:37 +0300

    'a'

--- example/testrx_tau.py
+++ example/testrx_tau.py
@@ -6,8 +6,17 @@ import threading
 from rxsignal import *
 
 t = rxrange(0, 100) * 0.01
-g = t.map(lambda x: 1) 
-s = [aperiodic_filter(g, 0.05*i, 0.01) for i in range(1, 40)]
+g = t.map(lambda a: 1)
 
-plot(t, *s)
+B = numpy.array([0,1])
+A = numpy.array([
+	[0,1],
+	[0.1,0.1]
+])
+
+out = dynsystem(g, A, B)
+
+x = out.map(lambda a: a[1])
+
+plot(t, x)
 show()
