156bb08734345e51c8d4f7030129e32299b6db4b
mirmik <mirmikns@yandex.ru>
2022-10-18 22:39:53 +0300

    'tau'

--- example/testrx_tau.py
+++ example/testrx_tau.py
@@ -2,21 +2,31 @@
 #coding: utf-8
 
 from zengraph import flowplot, plot, show
-import threading
 from rxsignal import *
 
-t = rxrange(0, 100) * 0.01
+step = 0.1
+t = rxrange(0, 100) * step 
 g = t.map(lambda a: 1)
 
-B = numpy.array([0,1])
 A = numpy.array([
-	[0,1],
-	[0.1,0.1]
+	[0, 1],
+	[-1, -1]
 ])
 
-out = dynsystem(g, A, B)
+B = numpy.array([
+	[0],
+	[-1]
+])
 
-x = out.map(lambda a: a[1])
+I = numpy.array([
+	[0],
+	[0]
+])
 
-plot(t, x)
+ds = DynamicSystem(g=g, A=A, B=B, step=step, init=I)
+
+x = ds.out().map(lambda a: a[0])
+v = ds.out().map(lambda a: a[1])
+
+plot(t, x, v)
 show()
